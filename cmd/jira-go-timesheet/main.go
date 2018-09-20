a7efdb22dc8197a8dfadd50cfa1357cb2ae8f916
Eugene Lavnikevich <lava@midgard.by>
2018-09-20 19:30:58 +0300

    main

---
+++ cmd/jira-go-timesheet/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "github.com/andygrunwald/go-jira"
+    "log"
+    "os"
+    "strings"
+)
+
+func main() {
+    // Read credentials from input
+    // Login to JIRA
+    // Get worklogs
+    // Format to CSV
+}
+
