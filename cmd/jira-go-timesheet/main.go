4b6077e187f0e459f2223c7d35313ca3f4ed3448
Eugene Lavnikevich <lava@midgard.by>
2018-09-25 17:34:08 +0300

    hello world

--- cmd/jira-go-timesheet/main.go
+++ cmd/jira-go-timesheet/main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-    "github.com/andygrunwald/go-jira"
-    "log"
+    "encoding/json"
+    "fmt"
     "os"
-    "strings"
+//    "github.com/andygrunwald/go-jira"
+//    "log"
+//    "os"
+//    "strings"
 )
 
+type Configuration struct {
+    Users    []string
+    Groups   []string
+}
+
 func main() {
+    fmt.Println("hello world")
+
+    file, _ := os.Open("~/conf.json")
+    defer file.Close()
+
+    decoder := json.NewDecoder(file)
+
+    configuration := Configuration{}
+    err := decoder.Decode(&configuration)
+    if err != nil {
+        fmt.Println("error:", err)
+        configuration.Users = ["test1"]
+        configuration.Groups = ["test2"]
+    }
+    fmt.Println(configuration.Users) // output: [UserA, UserB]
+
     // Read credentials from input
     // Login to JIRA
     // Get worklogs
