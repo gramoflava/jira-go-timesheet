523acfbb20cefc588d3ec00a52e262e81545da82
Eugene Lavnikevich <lava@midgard.by>
2018-10-17 23:11:42 +0300

    update to golang style guides

--- cmd/jira-go-timesheet/main.go
+++ cmd/jira-go-timesheet/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"github.com/andygrunwald/go-jira"
 	"log"
 	"os"
 	"time"
+
+	"github.com/andygrunwald/go-jira"
 )
 
-const SecondsPerHour = 3600
+const secondsPerHour = 3600
+const configFilePath = "~/.config/jira-go-timesheet/config.json"
 
-type Config struct {
+type config struct {
 	URL       string
 	BaseJQL   string
 	Login     string
@@ -21,21 +23,22 @@ type Config struct {
 	CSVOut    string
 }
 
-func GetConfig() Config {
+func getConfig() config {
 	//TODO: Add reading from the actual config file
 	//https://www.thepolyglotdeveloper.com/2017/04/load-json-configuration-file-golang-application/
-	var config Config
-	config.URL = "https://tandbergdata.atlassian.net"
-	config.BaseJQL = "PROJECT in (RDX, VTX2U, VTX1U) AND worklogAuthor in (v.redzhepov, vshakhov, a.hrytsevich) AND timespent is not EMPTY"
-	config.Login = "e.lavnikevich@sam-solutions.com"
-	config.Password = "Js6Us47UtB78qcsY9[qP"
-	config.DateFrom = "2018-09-01"
-	config.DateUntil = "2018-09-30"
-	config.CSVOut = "/Users/lava/Timesheet.csv"
-	return config
+
+	var cfg config
+	cfg.URL = "https://tandbergdata.atlassian.net"
+	cfg.BaseJQL = "PROJECT in (RDX, VTX2U, VTX1U) AND worklogAuthor in (v.redzhepov, vshakhov, a.hrytsevich) AND timespent is not EMPTY"
+	cfg.Login = "e.lavnikevich@sam-solutions.com"
+	cfg.Password = "Js6Us47UtB78qcsY9[qP"
+	cfg.DateFrom = "2018-09-01"
+	cfg.DateUntil = "2018-09-30"
+	cfg.CSVOut = "/Users/lava/Timesheet.csv"
+	return cfg
 }
 
-func WriteCSV(path string, records [][]string) {
+func writeCSV(path string, records [][]string) {
 	file, _ := os.Create(path)
 	defer file.Close()
 
@@ -55,19 +58,19 @@ func WriteCSV(path string, records [][]string) {
 }
 
 func main() {
-	config := GetConfig()
+	cfg := getConfig()
 	log.Println("Got configuration.")
 
 	tp := jira.BasicAuthTransport{
-		Username: config.Login,
-		Password: config.Password,
+		Username: cfg.Login,
+		Password: cfg.Password,
 	}
 
-	client, _ := jira.NewClient(tp.Client(), config.URL)
+	client, _ := jira.NewClient(tp.Client(), cfg.URL)
 	log.Println("Connection established.")
-	log.Printf("Running query: '%s'.\n", config.BaseJQL)
+	log.Printf("Running query: '%s'.\n", cfg.BaseJQL)
 
-	issues, _, _ := client.Issue.Search(config.BaseJQL, nil)
+	issues, _, _ := client.Issue.Search(cfg.BaseJQL, nil)
 	log.Printf("%d issues found.\n", len(issues))
 
 	timesheets := make([][]string, 0)
@@ -94,7 +97,7 @@ func main() {
 			}
 			timesheets[i] = append(timesheets[i], fmt.Sprintf("%s", record.Author.DisplayName))
 			timesheets[i] = append(timesheets[i], fmt.Sprintf("%d.%d.%d", started.Day(), started.Month(), started.Year()))
-			timesheets[i] = append(timesheets[i], fmt.Sprintf("%.1f", float64(record.TimeSpentSeconds)/SecondsPerHour))
+			timesheets[i] = append(timesheets[i], fmt.Sprintf("%.1f", float64(record.TimeSpentSeconds)/secondsPerHour))
 			timesheets[i] = append(timesheets[i], fmt.Sprintf("%s", record.Comment))
 
 			i++
@@ -103,5 +106,5 @@ func main() {
 	log.Println("Done.")
 	log.Println("Dumping CSV.")
 
-	WriteCSV(config.CSVOut, timesheets)
+	writeCSV(cfg.CSVOut, timesheets)
 }
